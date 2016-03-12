8d6880352d6bee0f4b50f5620d05d9b80437c8e1
Dawid <dcz31249@gmail.com>
2016-03-12 17:15:40 +0100

    UI for footer is DONE

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './css/navigation.css';
 import './css/header.css';
 import './css/about.css';
 import './css/contact.css';
+import './css/footer.css';
+
 
 import jquery from 'jquery';
 import 'imports?jQuery=jquery!./js/bootstrap.js';
