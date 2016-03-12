9d368ed78c9bbfc232a9443b4702197ce94be9bf
Dawid <dcz31249@gmail.com>
2016-03-12 14:04:31 +0100

    UI for contact section done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/bootstrap.css';
 import './css/navigation.css';
 import './css/header.css';
 import './css/about.css';
+import './css/contact.css';
 
 import jquery from 'jquery';
 import 'imports?jQuery=jquery!./js/bootstrap.js';
