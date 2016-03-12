7bf061e27568db8bd5f2833f8a1abe54ee0d81ef
Dawid <dcz31249@gmail.com>
2016-03-12 01:07:40 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/app.css';
+import './css/bootstrap.css';
+import './css/navigation.css';
+import './css/header.css';
+import './css/about.css';
+
+import jquery from 'jquery';
+import 'imports?jQuery=jquery!./js/bootstrap.js';
---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+var moduleLoaders = [
+  {
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+  },
+  {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'babel', 
+    query: {
+      presets: ['es2015']
+    }
+  },
+  {
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+    ]
+  },
+  {
+    test: /\.html$/,
+    loader: 'html?interpolate'
+  },
+  {
+    test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+    loader: 'file-loader?name=fonts/[name].[ext]'
+  }
+];
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new ExtractTextPlugin("styles.css"),
+  ],
+  module: {
+    loaders: moduleLoaders
+  }
+};
