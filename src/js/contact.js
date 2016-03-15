8a6e0d67571ce20c59cfe97a8b1875c5fb36b901
dawid <dcz31249@gmail.com>
2016-03-15 20:37:10 +0100

    contact form interactions improvement

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,8 +4,20 @@ export const $form = $('.contact-form-container form');
 
 var $formSubjectField = $('#contact-select-subject');
 var $formMessageField = $('#contact-textarea-message');
+var $formRegistrationNumberField = $('#contact-input-register');
 
 export function fillContactForm (subjectIndex, message) {
   $formMessageField.val(message);
   $formSubjectField.val(subjectIndex).change();
 }
+
+$formSubjectField.on('change', function () {
+  var selectedOption = $formSubjectField.find(':selected').text();
+
+  if(selectedOption === 'Business') {
+    $formRegistrationNumberField.parent().hide();
+  } else {
+    $formRegistrationNumberField.parent().show();
+  }
+
+});
